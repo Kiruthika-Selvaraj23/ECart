b3f287464330aec8f64e780953c065fc835c779e
Kiruthika-Selvaraj23 <keerthiselvaraj2003@gmail.com>
2026-03-25 13:08:09 +0530

    updated files

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,9 +1,10 @@
-import { useNavigate } from 'react-router'
+import { Navigate, useNavigate } from 'react-router'
 
 export default function ProtectedRoute({ user, allowedRoles, children }) {
     const navigate = useNavigate()
     if (!user)
-        return navigate("/")
+        // return navigate("/")
+        return <Navigate to="/" replace />;
 
     if (!allowedRoles.includes(user.role)) {
         if (user.role === "user") {
